60d2311bef34fe8d6221a04ca6e2f7f6e622962c
shouko.- <144153287+skotish06@users.noreply.github.com>
2025-08-29 16:18:29 +0700

    hosting

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: false,
+        allowedHosts: ['shouko.site', 'localhost']
+    }
 })
